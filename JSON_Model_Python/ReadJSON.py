d8747948912b7685e74f784ca4d2ac152c2692b0
Varun Sanjay <varun.sanjay@multicorewareinc.com>
2023-03-14 18:48:52 +0530

    UPdated code for ReadJSON.py

--- JSON_Model_Python/ReadJSON.py
+++ JSON_Model_Python/ReadJSON.py
@@ -5,26 +5,39 @@ f = open('trace_inception.json')
 data = json.load(f)
 
 lst = data['traceEvents']
-keyList = ['_Zen','Zen']
-res = [d for d in lst for key in keyList if key in d['name']]
+
 keys = ['name','dur']
 newlst = []
-for d in res:
+for d in lst:
   newres = {}
-  for key in keys:
-    newres[key]=d[key]
-  newlst.append(newres)
+  if 'dur' in d:
+    for key in keys:
+      newres[key]=d[key]
+    newlst.append(newres)
+  else:
+    continue
 
 newlst1={}
-
+total = 0
 for d in newlst:
   if d['name'] not in newlst1.keys():
     newlst1[d['name']]=0
   newlst1[d['name']]+=int(d['dur'])
+  total+=int(d['dur'])
 
+newlst1=dict(sorted(newlst1.items(), key=lambda x:x[1], reverse=True))
+total=total/1000
+tmplst = newlst1.copy()
 for key in newlst1:
   newlst1[key]/=1000
-  newlst1[key]=str(newlst1[key])+' ms'	    
+  newlst1[key]=str(newlst1[key])+'ms'	    
 print(newlst1)
+
+print('Percentage List for each layer')
+for key in tmplst:
+  tmplst[key]/=1000
+  print(key,' :',(tmplst[key]/total)*100,'%')
+
+print('Total Time Taken: ',total,'ms')
 
 f.close()
